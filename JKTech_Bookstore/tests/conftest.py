4c8c8fb39b625f464bc0cf5806ba6256e9a337bb
AArunvardhan <108665234+AArunvardhan@users.noreply.github.com>
2025-02-20 00:08:39 +0530

    Update conftest.py

--- JKTech_Bookstore/tests/conftest.py
+++ JKTech_Bookstore/tests/conftest.py
@@ -3,7 +3,7 @@ import pytest
 from fastapi.testclient import TestClient
 from httpx import AsyncClient
 from motor.motor_asyncio import AsyncIOMotorClient
-from JKTech_Project.JKTech_Bookstore.my_bookstore.main import app
+from JKTech_Bookstore.my_bookstore.main import app
 from JKTech_Project.JKTech_Bookstore.my_bookstore.database import get_db
 from JKTech_Project.JKTech_Bookstore.my_bookstore.constants import TEST_MONGODB_URI
 
